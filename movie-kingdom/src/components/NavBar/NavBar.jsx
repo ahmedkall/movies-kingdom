04b9acaecf03db86767302ae5762fa10c7399c19
haouetya <yassinehaouet1234@gmail.com>
2022-11-26 00:18:27 +0100

    Profile functionality added.

--- movie-kingdom/src/components/NavBar/NavBar.jsx
+++ movie-kingdom/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { AppBar, IconButton, Toolbar, Drawer , Button, Avatar , useMediaQuery} from '@mui/material';
 import {Menu, AccountCircle, Brightness4, Brightness7} from '@mui/icons-material'
 import { Link } from 'react-router-dom';
 import useStyles from './style'
 import { useTheme} from '@mui/material/styles';
-import Search from '../Search/Search';
 import Sidebar from '../SideBar/Sidebar';
+import Search from '../Search/Search';
+import { fetchToken, moviesApi, createSessionID } from '../../utils';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUser,userSelector } from '../../features/authentication';
 
 
 const NavBar = () => {
+    
     const classes = useStyles();
     const [mobileOpen, setMobileOpen] = useState(false);
     const isMobile = useMediaQuery('(max-width:600px')
     const theme = useTheme();
-    const isAutheticated= true;
+    const dispatch = useDispatch();
+    const {isAuthenticated,user}= useSelector(userSelector);
+    
+    const token = localStorage.getItem('request_token');
+    const sessionIDfromLS = localStorage.getItem('session_id');
+
+    useEffect(() => {
+        const logInUser = async ()=>{ 
+            if(token){
+                if(sessionIDfromLS){
+                    const {data: userData}= await moviesApi.get( `/account?session_id=${sessionIDfromLS}`);
+                    dispatch(setUser(userData));
+
+                }else{
+                    const sessionId = await createSessionID();
+                    const {data: userData}= await moviesApi.get( `/account?session_id=${sessionId}`);
+                    dispatch(setUser(userData));
+
+                }
+            }
+        }
+        logInUser();
+      }, [token])
+    
   return (
     <>
         <AppBar positon = 'fixed'>
@@ -32,12 +59,10 @@ const NavBar = () => {
                 <IconButton color = 'inherit' sx= {{ ml: 1 } } onClick = {() => {}}>  {/* ml= margin left */}
                     {theme.palette.mode === 'dark' ? <Brightness7/> : <Brightness4/> }
                 </IconButton>
-
-                {!isMobile && <Search/> }
-
+                {!isMobile && <Search/>}
                 <div>
-                    {!isAutheticated ? ( 
-                        <Button color = 'inherit' onClick={() => {}}>
+                    {!isAuthenticated ? ( 
+                        <Button color = 'inherit' onClick={fetchToken}>
                             Login &nbsp; <AccountCircle/> {/*&nbsp = space*/} 
                         </Button>
 
@@ -45,7 +70,7 @@ const NavBar = () => {
                         <Button
                             color = 'inherit' 
                             component= {Link} 
-                            to= {`/profile/:id`}
+                            to= {`/profile/${user.id}`}
                             className= {classes.linkButton}
                             onClick = {()=>{}}
                             >
@@ -62,9 +87,7 @@ const NavBar = () => {
 
                     )}
                 </div>
-
                 {isMobile && <Search/>}
-
             </Toolbar>
         </AppBar> 
         {/*sidebar*/}
--- movie-kingdom/src/components/Profile/Profile.jsx
+++ movie-kingdom/src/components/Profile/Profile.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-
+import React ,{useEffect} from 'react';
+import { Typography,Button,Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../features/authentication';
 const Profile = () => {
+  const favoriteMovies= []
+  const user = useSelector(userSelector);
+  const logOut = () => {
+    localStorage.clear(); 
+    window.location.href='/';
+  }
+  console.log(user);
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display= 'flex' justifyContent='space-between'>
+        <Typography variant='h4' gutterBottom>My Profile</Typography>
+        <Button color= 'inherit' onClick={logOut}>
+          Logout &nbsp; <ExitToApp/>
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? <Typography variant='h5'>Add Favorities or WatchList some Movies to see them here.</Typography>:<Box> Favorite Movies </Box>}
+    </Box>
   );
 }
 
