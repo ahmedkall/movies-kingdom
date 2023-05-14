50193fe33c3c1f09b0cd82e299a1b579e29e115f
oussamf1 <oussamafathallah854@gmail.com>
2023-05-14 13:31:48 +0200

    fixed redux query middleware

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from "../services/TMDB";
-import  genreOrCategoryReducer  from "../features/currentGenreOrCategory";
+import genreOrCategoryReducer from "../features/currentGenreOrCategory";
 import userReducer from "../features/authentication";
 
-
 export default configureStore({
-    reducer:{
-        [tmdbApi.reducerPath]: tmdbApi.reducer,
-        currentGenreOrCategory : genreOrCategoryReducer,
-        user: userReducer,
-    }
-
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+    currentGenreOrCategory: genreOrCategoryReducer,
+    user: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tmdbApi.middleware),
 });
