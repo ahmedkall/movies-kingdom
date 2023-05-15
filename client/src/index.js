c13177769305ca49569ebcf5516558182f386ffe
oussamf1 <oussamafathallah854@gmail.com>
2023-05-15 21:37:25 +0200

    removed hard coded secrets

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import {BrowserRouter} from 'react-router-dom'
-import {createTheme , ThemeProvider} from '@mui/material/styles'
-import {Provider} from 'react-redux'
-import ToggleDarkMode from './utils/ToggleDarkMode';
-import store from './app/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import ToggleDarkMode from "./utils/ToggleDarkMode";
+import store from "./app/store";
 
 const theme = createTheme({});
 
 ReactDOM.render(
-    <Provider store={store}>
-      <ToggleDarkMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <App />
-        </BrowserRouter>
-      </ToggleDarkMode>
-    </Provider>,
-    document.getElementById('root')
-  );
-  
+  <Provider store={store}>
+    <ToggleDarkMode>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+      </BrowserRouter>
+    </ToggleDarkMode>
+  </Provider>,
+  document.getElementById("root")
+);
