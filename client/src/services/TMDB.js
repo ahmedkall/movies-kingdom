b5af4f0163a5103134a1cb6e5bc48af884deac29
oussamf1 <oussamafathallah854@gmail.com>
2023-05-15 21:35:23 +0200

    removed hard coded secrets

--- client/src/services/TMDB.js
+++ client/src/services/TMDB.js
@@ -1,78 +1,80 @@
-import {createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-const tmdbApiKey = '0067f4c1a8157e1b0096a428d72e2b61';
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 
 export const tmdbApi = createApi({
-    reducerPath: 'tmdbApi',
-    baseQuery: fetchBaseQuery({ baseUrl:'https://api.themoviedb.org/3'}),
-    endpoints: (builder)=>({
-        //* Get Genres
-        getGenres: builder.query({
-            query: ()=> `genre/movie/list?api_key=${tmdbApiKey}`,
-        }),
-        //* Get Movies by [Type] 
-        getMovies: builder.query ({
-            query: ({genreIdOrCategoryName,page, searchQuery}) =>  {
-
-                //*Get Movies By Search 
-                if(searchQuery){
-                    console.log(searchQuery,page);
-
-                    return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`
-                }
-
-                //*Get Movies by Category
-                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string'){
-                    console.log(typeof genreIdOrCategoryName,page);
-
-                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
-
-                }
-                //*Get Movies by Genre
-                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number'){
-                    console.log(typeof genreIdOrCategoryName, page);
-                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
-
-                }
-
-                //*Get Popular movies   
-                return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
-                
-            }
-            
-        }),
-        //* Get Movie 
-        getMovie: builder.query({
-            query: (id)=> `/movie/${id}?append_to_response=videos&api_key=${tmdbApiKey}`
-        }),
-        //*Get Credits
-        getCredits: builder.query({
-            query: (id)=> `/movie/${id}/credits?api_key=${tmdbApiKey}`
-        }),
-        getSimilarMovie: builder.query({
-            query: (id)=> `/movie/${id}/similar?api_key=${tmdbApiKey}`,
-        }),
-        getActor: builder.query({
-            query: (id)=> `/person/${id}?api_key=${tmdbApiKey}`,
-        }),
-        //https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=<<api_key>>&language=en-US
-        getMovieCredits: builder.query({
-            query: (id)=> `/person/${id}/movie_credits?api_key=${tmdbApiKey}`,
-        }),
-        //get user specific movie list
-        getList: builder.query({
-            query:( {listName,accountId,sessionId,page} )=>`/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`
-        })
+  reducerPath: "tmdbApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
+  endpoints: (builder) => ({
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
+    //* Get Movies by [Type]
+    getMovies: builder.query({
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //*Get Movies By Search
+        if (searchQuery) {
+          console.log(searchQuery, page);
+
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
+        //*Get Movies by Category
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "string"
+        ) {
+          console.log(typeof genreIdOrCategoryName, page);
+
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+        }
+        //*Get Movies by Genre
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "number"
+        ) {
+          console.log(typeof genreIdOrCategoryName, page);
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
+        //*Get Popular movies
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
+    //* Get Movie
+    getMovie: builder.query({
+      query: (id) =>
+        `/movie/${id}?append_to_response=videos&api_key=${tmdbApiKey}`,
+    }),
+    //*Get Credits
+    getCredits: builder.query({
+      query: (id) => `/movie/${id}/credits?api_key=${tmdbApiKey}`,
+    }),
+    getSimilarMovie: builder.query({
+      query: (id) => `/movie/${id}/similar?api_key=${tmdbApiKey}`,
+    }),
+    getActor: builder.query({
+      query: (id) => `/person/${id}?api_key=${tmdbApiKey}`,
+    }),
+    //https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=<<api_key>>&language=en-US
+    getMovieCredits: builder.query({
+      query: (id) => `/person/${id}/movie_credits?api_key=${tmdbApiKey}`,
+    }),
+    //get user specific movie list
+    getList: builder.query({
+      query: ({ listName, accountId, sessionId, page }) =>
+        `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
+    }),
+  }),
 });
 
 export const {
-    useGetGenresQuery,
-    useGetMoviesQuery,
-    useGetMovieQuery,
-    useGetCreditsQuery,
-    useGetSimilarMovieQuery,
-    useGetActorQuery,
-    useGetMovieCreditsQuery,
-    useGetListQuery
-}= tmdbApi;
+  useGetGenresQuery,
+  useGetMoviesQuery,
+  useGetMovieQuery,
+  useGetCreditsQuery,
+  useGetSimilarMovieQuery,
+  useGetActorQuery,
+  useGetMovieCreditsQuery,
+  useGetListQuery,
+} = tmdbApi;
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,39 +1,38 @@
+import { EventNote } from "@mui/icons-material";
 import axios from "axios";
 export const moviesApi = axios.create({
-    baseURL: 'https://api.themoviedb.org/3',
-    params:{
-        api_key: '0067f4c1a8157e1b0096a428d72e2b61',
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: process.env.REACT_APP_TMDB_KEY,
+  },
+});
+
+export const fetchToken = async () => {
+  try {
+    const { data } = await moviesApi.get("/authentication/token/new");
+    const token = data.request_token;
+    if (data.success) {
+      localStorage.setItem("request_token", token);
+      window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`;
     }
-})
+  } catch (error) {
+    console.log("sorry failed to create your token.");
+  }
+};
 
-export const fetchToken = async () =>{
-    try{
-        const {data }= await moviesApi.get('/authentication/token/new');
-        const token = data.request_token;
-        if(data.success){
-            localStorage.setItem('request_token',token);
-            window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`;
-        }
-        
-    }catch (error){ console.log('sorry failed to create your token.') }
-
-}
-
-
-
-export const createSessionID = async () =>{
-    const token=localStorage.getItem('request_token');
-    if(token){
-        try {
-            const {data: {session_id}} = await moviesApi.post('authentication/session/new',{
-                request_token: token,
-            }); 
-            localStorage.setItem('session_id',session_id)
-            return session_id;
-            
-        } catch (error) {
-            console.log(error);
-            
-        }
+export const createSessionID = async () => {
+  const token = localStorage.getItem("request_token");
+  if (token) {
+    try {
+      const {
+        data: { session_id },
+      } = await moviesApi.post("authentication/session/new", {
+        request_token: token,
+      });
+      localStorage.setItem("session_id", session_id);
+      return session_id;
+    } catch (error) {
+      console.log(error);
     }
-}
+  }
+};
